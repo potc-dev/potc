cb66a3cf21d7b4673148739a3d70958e74ff7b91
HansBug <killog@126.com>
2021-08-11 23:46:39 +0800

    init the project

---
+++ potc/config/meta.py
@@ -0,0 +1,19 @@
+"""
+Overview:
+    Meta information for potc package.
+"""
+
+#: Title of this project (should be `potc`).
+__TITLE__ = "potc"
+
+#: Version of this project.
+__VERSION__ = "0.0.1"
+
+#: Short description of the project, will be included in ``setup.py``.
+__DESCRIPTION__ = 'Python object to code module.'
+
+#: Author of this project.
+__AUTHOR__ = "HansBug"
+
+#: Email of the authors'.
+__AUTHOR_EMAIL__ = "hansbug@buaa.edu.cn"
