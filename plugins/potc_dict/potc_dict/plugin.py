84e35aa4c3fb68c1f6a1d05a27b630a539bcb012
HansBug <killog@126.com>
2021-08-19 20:15:58 +0800

    update definition of __rules__, make necessary comments

--- plugins/potc_dict/potc_dict/plugin.py
+++ plugins/potc_dict/potc_dict/plugin.py
@@ -1,7 +1,7 @@
 import re
 
 from potc.fixture import rule, Addons
-from potc.rules import builtin_dict
+from potc.rules import builtin_dict, builtin_object
 
 _KEY_PATTERN = re.compile('^[_a-zA-Z][_0-9a-zA-Z]*$')
 
@@ -15,6 +15,11 @@ def pretty_dict(v: dict, addon: Addons):
     return addon.obj(type(v))(**{key: value for key, value in v.items()})
 
 
-__rules__ = [
-    (pretty_dict, builtin_dict)
-]
+__rules__ = (
+    [
+        # pretty_dict will be executed before builtin_dict
+        (pretty_dict, builtin_dict,),
+    ],
+    # add this to make sure that all the new rules will be executed before builtin_object
+    builtin_object,
+)
