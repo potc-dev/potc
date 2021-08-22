9c1e6434d576e0c7251a0d11a8f84b7bc899c94b
HansBug <killog@126.com>
2021-08-22 22:31:58 +0800

    complete potc.fixture.addons

--- docs/source/api_doc/fixture/addons.rst
+++ docs/source/api_doc/fixture/addons.rst
@@ -1,5 +1,16 @@
 potc.fixture.addons
 ======================
 
+Addons
+---------------
 
+.. autoclass:: potc.fixture.addons.Addons
+    :members: __init__, obj, val, rule, import_items, unprocessable, transaction, __getstate__, __setstate__
+
+
+AddonProxy
+---------------
+
+.. autoclass:: potc.fixture.addons.AddonProxy
+    :members: __init__, __getattr__, __getitem__, __call__, __str__
 
--- potc/fixture/addons.py
+++ potc/fixture/addons.py
@@ -2,7 +2,7 @@ import copy
 from contextlib import contextmanager
 from functools import lru_cache
 from itertools import chain
-from typing import Tuple
+from typing import Tuple, Optional
 
 from .imports import ImportPool, ImportStatement, FromImport, DirectImport
 from ..utils import try_import_info
@@ -19,14 +19,49 @@ def unprocessable():
 
 
 class AddonProxy:
+    """
+    Overview:
+        Proxy of addon expression.
+    """
+
     def __init__(self, base, rule):
+        """
+        Overview:
+            Constructor of ``AddonProxy``
+
+        Arguments:
+            - base: Base unit of the proxy.
+            - rule: Translation rule.
+        """
         self.__base = base
         self.__rule = rule
 
     def __getattr__(self, item: str):
+        """
+        Overview:
+            Generate attribute-getting expression, \
+            like ``base.xxx``.
+
+        Arguments:
+            - item (:obj:`str`): Attribute name.
+
+        Returns:
+            - proxy: New generated proxy.
+        """
         return AddonProxy(f'{self.__base}.{item}', self.__rule)
 
     def __getitem__(self, item):
+        """
+        Overview:
+            Generate item-getting expression, \
+            like ``base[233]``, ``base[::-1]`` or ``base[1, 2, 3]``.
+
+        Arguments:
+            - item (:obj:`str`): Item value, can be simple value, slice or tuple.
+
+        Returns:
+            - proxy: New generated proxy.
+        """
         if isinstance(item, slice):
             _items = [item.start, item.stop, *((item.step,) if item.step is not None else ())]
             _item_str = ':'.join(map(lambda x: '' if x is None else self.__rule(x), _items))
@@ -38,6 +73,18 @@ class AddonProxy:
         return AddonProxy(f'{self.__base}[{_item_str}]', self.__rule)
 
     def __call__(self, *args, **kwargs):
+        """
+        Overview:
+            Generate item call expression, \
+            like ``base(1, 2, x=1, y=2)``.
+
+        Arguments:
+            - args: Positional arguments.
+            - kwargs: Key-word arguments.
+
+        Returns:
+            - proxy: New generated proxy.
+        """
         _items = chain(
             map(lambda x: self.__rule(x), args),
             map(lambda p: f"{p[0]}={self.__rule(p[1])}", kwargs.items())
@@ -48,11 +95,31 @@ class AddonProxy:
         )
 
     def __str__(self) -> str:
+        """
+        Overview:
+            String format of expression.
+
+        Returns:
+            - return (:obj:`str`): String formatted expression.
+        """
         return self.__base
 
 
 class Addons:
+    """
+    Overview:
+        Addon object used in rules.
+    """
+
     def __init__(self, rule=None):
+        """
+        Overview:
+            Constructor of ``Addons``.
+
+        Arguments:
+            - rule: Translation rule, default is ``None`` which means no translation rule, \
+                auto translation will also be disabled when ``None`` is used.
+        """
         self.__imports = ImportPool()
         self.__rule = rule
 
@@ -62,7 +129,22 @@ class Addons:
     def __from(self, _from: str) -> FromImport:
         return self.__imports.from_(_from)
 
-    def obj(self, obj, alias=None) -> AddonProxy:
+    def obj(self, obj, alias: Optional[str] = None) -> AddonProxy:
+        """
+        Overview:
+            Generate an object proxy.
+            Object proxy means a object which need to be automatically imported, \
+            such as ``json``, an import statement ``import json`` will be generated.
+            If the object cannot be imported, it will raise exception.
+
+        Arguments:
+            - obj: Target object.
+            - alias (:obj:`Optional[str]`): Alias name of the imported object, \
+                default is ``None`` which means do not alias.
+
+        Returns:
+            - proxy (:obj:`AddonProxy`): Expression proxy object.
+        """
         with self.transaction():
             segments = try_import_info(obj, alias=alias)
             if alias is None and segments[:2] == ('from', 'builtins'):
@@ -82,23 +164,70 @@ class Addons:
             return AddonProxy(str(_import.target), self.rule)
 
     def val(self, obj) -> AddonProxy:
+        """
+        Overview:
+            Generate a value proxy.
+            Differs from ``obj`` method, this method will not do automatic import, \
+            it will just generate the string expression with the rule translator.
+
+        Arguments:
+            - obj: Value object.
+
+        Returns:
+            - proxy (:obj:`AddonProxy`): Expression proxy object.
+        """
         with self.transaction():
             return AddonProxy(self.rule(obj), self.rule)
 
-    def rule(self, v):
+    def rule(self, v) -> str:
+        """
+        Overview:
+            Express the value with the rule translator.
+
+        Arguments:
+            - v: Value object.
+
+        Returns:
+            - str (:obj:`str`): String formatted expression.
+        """
         with self.transaction():
             return str(self.__rule(v, self))
 
     @property
     def import_items(self) -> Tuple[ImportStatement, ...]:
+        """
+        Overview:
+            Import statements.
+
+        Returns:
+            - statements (:obj:`Tuple[ImportStatement, ...]`): Tuple of import statements.
+        """
         return self.__imports.imports
 
     # noinspection PyMethodMayBeStatic
     def unprocessable(self):
+        """
+        Overview:
+            Raise an exception ``UnprocessableError``.
+            Announce that the current rule cannot express this value.
+        """
         unprocessable()
 
     @contextmanager
     def transaction(self):
+        """
+        Overview:
+            Open up an atomic transaction context.
+            If exception is thrown in this context, all the update in this \
+            addon object will be undone.
+
+        Example:
+            >>> with addon.transaction():
+            >>>     addon.rule(1)
+            >>>     addon.val([1, 2])
+            >>>     addon.obj(json)
+            >>>     raise RuntimeError  # the changes above will be undone
+        """
         _state = copy.deepcopy(self.__getstate__())
         try:
             yield self
@@ -107,7 +236,21 @@ class Addons:
             raise err
 
     def __getstate__(self):
+        """
+        Overview:
+            Get state value of this object.
+
+        Returns:
+            - state: State value.
+        """
         return self.__imports, self.__rule
 
     def __setstate__(self, state):
+        """
+        Overview:
+            Set state value of this object.
+
+        Arguments:
+            - state: State value.
+        """
         self.__imports, self.__rule = state
