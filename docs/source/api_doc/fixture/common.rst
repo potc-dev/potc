d9c17f1b3fe290622e1fa61c11fb0db927c2ae98
HansBug <killog@126.com>
2021-08-22 15:08:12 +0800

    add documentation of potc.fixture.common

--- docs/source/api_doc/fixture/common.rst
+++ docs/source/api_doc/fixture/common.rst
@@ -1,5 +1,38 @@
 potc.fixture.common
 ======================
 
+UnprocessableError
+------------------------
 
+.. autoclass:: potc.fixture.common.UnprocessableError
+
+
+unprocessable
+---------------------
+
+.. autofunction:: potc.fixture.common.unprocessable
+
+
+rule
+---------------------
+
+.. autofunction:: potc.fixture.common.rule
+
+
+is_rule
+---------------------
+
+.. autofunction:: potc.fixture.common.is_rule
+
+
+rule_name
+---------------------
+
+.. autofunction:: potc.fixture.common.rule_name
+
+
+rule_origin
+---------------------
+
+.. autofunction:: potc.fixture.common.rule_origin
 
--- potc/fixture/common.py
+++ potc/fixture/common.py
@@ -1,16 +1,24 @@
 import re
 from collections import namedtuple
 from functools import wraps
-from typing import Callable, Type, Union, Tuple
+from typing import Callable, Type, Union, Tuple, Optional
 
 from ..utils import dynamic_call
 
 
 class UnprocessableError(Exception):
+    """
+    Overview:
+        Signal exception which means the rules before cannot process this data.
+    """
     pass
 
 
 def unprocessable():
+    """
+    Overview:
+        Raise an ``UnprocessableError``.
+    """
     raise UnprocessableError()
 
 
@@ -20,7 +28,49 @@ _RULE_TAG = '__rule__'
 _NAME_PATTERN = re.compile('^[a-zA-Z0-9_]+$')
 
 
-def rule(alias=None, type_: Union[Type, Tuple[Type, ...], None] = None):
+def rule(alias: Optional[str] = None, type_: Union[Type, Tuple[Type, ...], None] = None):
+    """
+    Overview:
+        Make a common function to a rule function.
+
+    Arguments:
+        - alias (:obj:`Optional[str]`): Alias name of this rule, default is ``None`` which means \
+            just use the name of the wrapped function.
+        - type\\_ (:obj:`Union[Type, Tuple[Type, ...], None]`): Type of the data, can be a type \
+            or a tuple of types, default is ``None`` which means no type limit.
+
+    Returns:
+        - decorator: A function decorator of the raw rule function.
+
+    Examples:
+        There are some example from builtin part.
+
+        - builtin_int (A very easy case)
+
+        >>> @rule(type_=int)
+        >>> def builtin_int(v: int):
+        >>>     return repr(v)
+
+        - builtin_float (A little complex case)
+
+        >>> @rule(type_=float)
+        >>> def builtin_float(v: float, addon: Addons):
+        >>>     if math.isinf(v):
+        >>>         return ('+' if v > 0 else '-') + str(addon.obj(math).inf)
+        >>>     elif math.isnan(v):
+        >>>         return addon.obj(math).nan
+        >>>     else:
+        >>>         if math.isclose(v, math.e):
+        >>>             return addon.obj(math).e
+        >>>         elif math.isclose(v, math.pi):
+        >>>             return addon.obj(math).pi
+        >>>         elif math.isclose(v, math.tau):
+        >>>             return addon.obj(math).tau
+        >>>         else:
+        >>>             return repr(v)
+
+    """
+
     def _decorator(func: Callable):
         if is_rule(func):
             _name = alias or rule_name(func)
@@ -46,12 +96,49 @@ def rule(alias=None, type_: Union[Type, Tuple[Type, ...], None] = None):
 
 
 def is_rule(func: Callable) -> bool:
+    """
+    Overview:
+        Check if a function is a potc rule.
+
+    Arguments:
+        - func (:obj:`Callable`): Function object.
+
+    Returns:
+        - is_rule (:obj:`bool`): Is a rule or not.
+
+    Examples:
+        >>> is_rule(builtin_int)            # True
+        >>> is_rule(lambda x, addon: None)  # False
+    """
     return isinstance(getattr(func, _RULE_TAG, None), RuleInfo)
 
 
-def rule_name(func: Callable) -> str:
-    return getattr(func, _RULE_TAG).name
+def rule_name(rule_: Callable) -> str:
+    """
+    Overview:
+        Name of the rule.
+
+    Arguments:
+        - rule\\_ (:obj:`Callable`): Rule object.
+
+    Returns:
+        - name (:obj:`str`): Name of the rule.
+
+    Examples:
+        >>> rule_name(builtin_int)  # 'builtin_int'
+    """
+    return getattr(rule_, _RULE_TAG).name
 
 
-def rule_origin(func: Callable) -> Callable:
-    return getattr(func, _RULE_TAG).origin
+def rule_origin(rule_: Callable) -> Callable:
+    """
+    Overview:
+        Original function of the rule.
+
+    Arguments:
+        - rule\\_ (:obj:`Callable`): Rule object.
+
+    Returns:
+        - func (:obj:`str`): Original function.
+    """
+    return getattr(rule_, _RULE_TAG).origin
